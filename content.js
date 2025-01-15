8f3f72f458f2b6216865660ddbf6bf226552f5ce
Junqi Wu <juwu@odoo.com>
2025-01-15 13:57:38 -0500

    [FIX] Add confirmation to full composer fixes #4

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
-odoo.define('@odoo_confirm/core/common/composer_patch', ['@web/core/utils/patch', '@mail/core/common/composer', '@web/core/confirmation_dialog/confirmation_dialog'], function (require) {
+odoo.define('@odoo_confirm_module/core/common/composer_patch', ['@web/core/utils/patch', '@mail/core/common/composer', '@web/core/confirmation_dialog/confirmation_dialog', '@web/views/view_button/view_button', '@web/core/registry'], function (require) {
     'use strict';
     let __exports = {};
     const { patch } = require("@web/core/utils/patch");
     const { Composer } = require("@mail/core/common/composer");
     const { ConfirmationDialog } = require("@web/core/confirmation_dialog/confirmation_dialog");
+    const { ViewButton } = require("@web/views/view_button/view_button");
+    const { registry, KeyNotFoundError } = require("@web/core/registry");
     patch(Composer.prototype, {
         async sendMessage() {
             if (this.props.type == "note") {
@@ -15,10 +17,55 @@ odoo.define('@odoo_confirm/core/common/composer_patch', ['@web/core/utils/patch'
                 confirmLabel: "Send",
                 confirm: async () => {
                     await super.sendMessage(...arguments);
-                },
-                cancel: () => { },
+                }
+                ,
+                cancel: () => { }
+                ,
             })
         }
     });
+    patch(ViewButton.prototype, {
+        onClick(ev) {
+            if (this.clickParams.name !== "action_send_mail" || this.props.record.data.subtype_is_log) {
+                return super.onClick(...arguments);
+            }
+            this.env.services.dialog.add(ConfirmationDialog, {
+                title: "Odoo Confirm 😺",
+                body: "Are you sure you want to send this message?",
+                confirmLabel: "Send",
+                confirm: () => {
+                    super.onClick(...arguments);
+                }
+                ,
+                cancel: () => { }
+                ,
+            })
+        }
+    });
+    try {
+        const MailComposerSendDropdown = registry.category("view_widgets").get("mail_composer_send_dropdown").component;
+        patch(MailComposerSendDropdown.prototype, {
+            async onClickSend() {
+                if (this.props.record.data.subtype_is_log) {
+                    return await super.onClickSend(...arguments);
+                }
+                this.env.services.dialog.add(ConfirmationDialog, {
+                    title: "Odoo Confirm 😺",
+                    body: "Are you sure you want to send this message?",
+                    confirmLabel: "Send",
+                    confirm: async () => {
+                        await super.onClickSend(...arguments);
+                    }
+                    ,
+                    cancel: () => { }
+                    ,
+                })
+            }
+        });
+    } catch (e) {
+        if (!e instanceof KeyNotFoundError) {
+            throw e;
+        }
+    }
     return __exports;
 });
--- odoo_confirm_module/static/src/core/common/composer_patch.js
+++ odoo_confirm_module/static/src/core/common/composer_patch.js
@@ -1,6 +1,8 @@
 import { patch } from "@web/core/utils/patch";
 import { Composer } from "@mail/core/common/composer";
 import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { ViewButton } from "@web/views/view_button/view_button";
+import { registry, KeyNotFoundError } from "@web/core/registry";
 
 patch(Composer.prototype, {
     async sendMessage() {
@@ -18,3 +20,46 @@ patch(Composer.prototype, {
         })
     }
 });
+
+patch(ViewButton.prototype, {
+    onClick(ev) {
+        if (this.clickParams.name !== "action_send_mail" || this.props.record.data.subtype_is_log) {
+            return super.onClick(...arguments);
+        }
+        this.env.services.dialog.add(ConfirmationDialog, {
+            title: "Odoo Confirm 😺",
+            body: "Are you sure you want to send this message?",
+            confirmLabel: "Send",
+            confirm: () => {
+                super.onClick(...arguments);
+            },
+            cancel: () => { },
+        })
+    }
+});
+
+try {
+    // MailComposerSendDropdown is not exported, so we get it from the registry
+    const MailComposerSendDropdown = registry.category("view_widgets").get("mail_composer_send_dropdown").component;
+    patch(MailComposerSendDropdown.prototype, {
+        async onClickSend() {
+            if (this.props.record.data.subtype_is_log) {
+                return await super.onClickSend(...arguments);
+            }
+            this.env.services.dialog.add(ConfirmationDialog, {
+                title: "Odoo Confirm 😺",
+                body: "Are you sure you want to send this message?",
+                confirmLabel: "Send",
+                confirm: async () => {
+                    await super.onClickSend(...arguments);
+                },
+                cancel: () => { },
+            })
+        }
+    });
+} catch (e) {
+    // Odoo versions before 18.0 don't have the MailComposerSendDropdown component
+    if (!e instanceof KeyNotFoundError) {
+        throw e;
+    }
+}
