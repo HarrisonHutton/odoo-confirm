c00fb9069d49152f171333ee4b7ac5f2c18cd92c
Junqi Wu <juwu@odoo.com>
2025-01-17 13:29:03 -0500

    [REF] General refactoring - more reusable code - made conditions in which we open the confirmation clearer - exlcuded livechat composer from the confirmation (mode != "extended) - also excluded odoo-sh editors from loading content

--- content.js
+++ content.js
@@ -6,60 +6,48 @@ odoo.define('@odoo_confirm_module/core/common/composer_patch', ['@web/core/utils
     const { ConfirmationDialog } = require("@web/core/confirmation_dialog/confirmation_dialog");
     const { ViewButton } = require("@web/views/view_button/view_button");
     const { registry, KeyNotFoundError } = require("@web/core/registry");
+    function addConfirmationDialog(callback) {
+        this.env.services.dialog.add(ConfirmationDialog, {
+            title: "Odoo Confirm 😺",
+            body: "Are you sure you want to send this message?",
+            confirmLabel: "Send",
+            confirm: callback,
+            cancel: () => { },
+        });
+    }
     patch(Composer.prototype, {
         async sendMessage() {
-            if (this.props.type == "note") {
-                return await super.sendMessage(...arguments);
-            }
-            this.env.services.dialog.add(ConfirmationDialog, {
-                title: "Odoo Confirm 😺",
-                body: "Are you sure you want to send this message?",
-                confirmLabel: "Send",
-                confirm: async () => {
+            if (this.props.type !== "note" && this.props.mode === "extended") {
+                addConfirmationDialog.call(this, async () => {
                     await super.sendMessage(...arguments);
-                }
-                ,
-                cancel: () => { }
-                ,
-            })
+                });
+            } else {
+                await super.sendMessage(...arguments);
+            }
         }
     });
     patch(ViewButton.prototype, {
         onClick(ev) {
-            if (this.clickParams.name !== "action_send_mail" || this.props.record.data.subtype_is_log) {
-                return super.onClick(...arguments);
-            }
-            this.env.services.dialog.add(ConfirmationDialog, {
-                title: "Odoo Confirm 😺",
-                body: "Are you sure you want to send this message?",
-                confirmLabel: "Send",
-                confirm: () => {
+            if (this.clickParams.name === "action_send_mail" && !this.props.record.data.subtype_is_log) {
+                addConfirmationDialog.call(this, () => {
                     super.onClick(...arguments);
-                }
-                ,
-                cancel: () => { }
-                ,
-            })
+                });
+            } else {
+                super.onClick(...arguments);
+            }
         }
     });
     try {
         const MailComposerSendDropdown = registry.category("view_widgets").get("mail_composer_send_dropdown").component;
         patch(MailComposerSendDropdown.prototype, {
             async onClickSend() {
-                if (this.props.record.data.subtype_is_log) {
-                    return await super.onClickSend(...arguments);
-                }
-                this.env.services.dialog.add(ConfirmationDialog, {
-                    title: "Odoo Confirm 😺",
-                    body: "Are you sure you want to send this message?",
-                    confirmLabel: "Send",
-                    confirm: async () => {
+                if (!this.props.record.data.subtype_is_log) {
+                    addConfirmationDialog.call(this, async () => {
                         await super.onClickSend(...arguments);
-                    }
-                    ,
-                    cancel: () => { }
-                    ,
-                })
+                    });
+                } else {
+                    await super.onClickSend(...arguments);
+                }
             }
         });
     } catch (e) {
