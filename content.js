86565047ab41e964cf7dde196bb24e733bf451fd
Junqi <juwu@odoo.com>
2026-06-30 12:02:00 -0400

    [IMP] saas-19.4: update for owl3

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ function loadOdooConfirm() {
         return;
     }
 
-    const supportedVersions = ['18.0', 'saas~18.2', 'saas~18.3', 'saas~18.4', '19.0', 'saas~19.1', 'saas~19.2', 'saas~19.3'];
+    const supportedVersions = ['18.0', 'saas~18.2', 'saas~18.3', 'saas~18.4', '19.0', 'saas~19.1', 'saas~19.2', 'saas~19.3', 'saas~19.4'];
     if (!supportedVersions.includes(version)) {
         console.log(`😿 Odoo Confirm does not support Odoo version ${version} (yet)
 Make a request at https://github.com/HarrisonHutton/odoo-confirm/issues`);
